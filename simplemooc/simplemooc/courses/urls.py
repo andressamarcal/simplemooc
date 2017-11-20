88130bfcb37bcdf6bd3a343c1b972c667001812c
Andressa Marçal <andressagb@gmail.com>
2017-11-19 21:34:25 -0300

    Form and Change url and views course

--- simplemooc/simplemooc/courses/urls.py
+++ simplemooc/simplemooc/courses/urls.py
@@ -3,5 +3,6 @@ import simplemooc.courses import Course
 
 urlpatterns = [
     url(r'^$', 'index', name='index'),
-    url(r'^(?P<pk>\d+)/$', 'details', name='details')
+    #url(r'^(?P<pk>\d+)/$', 'details', name='details'),
+    url(r'^(?P<slug>[\w_-]+)/$', 'details', name='details')
 ]
--- simplemooc/simplemooc/courses/views.py
+++ simplemooc/simplemooc/courses/views.py
@@ -1,16 +1,31 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Course
+from .forms import ContactCourse
 
 def index(request):
     courses = Courses.objects.all() #queryset
-    template_name = 'courses/courses.html'
+    template_name = 'courses/index.html'
     context = {
         'courses': courses
     }
     return render(request, template_name, context)
 
-def details(request, pk):
-    pass
+'''def details(request, pk):
+    course = get_object_or_404(Course, pk=pk)
+    template_name = 'courses/details.html'
+    context = {
+        'course': course
+    }
+    return render(request, template_name, context)'''
+
+def details(request, slug):
+    course = get_object_or_404(Course, slug=slug)
+    template_name = 'courses/details.html'
+    context = {
+        'course': course,
+        'form': ContactCourse()
+    }
+    return render(request, template_name, context)
